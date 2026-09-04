4cee6e913f4bbad483bff46732126972b98e10c5
Türker Kıvılcım <kivturker@gmail.com>
2026-09-04 12:57:43 +0300

    add: implement main flow

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/turkerkiv/gator/internal/config"
 )
@@ -12,13 +13,34 @@ func main() {
 		fmt.Println(err)
 	}
 
-	fmt.Println(cfg.CurrentUserName, cfg.DbUrl)
-	cfg.SetUser("turker")
-
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Println(err)
+	appState := state{
+		config: &cfg,
 	}
 
-	fmt.Println(cfg.CurrentUserName, cfg.DbUrl)
+	commands := newCommands()
+	commands.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("there must be at least a command and potentially arguments")
+		os.Exit(1)
+	}
+	userCommand := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
+
+	if err := commands.run(&appState, userCommand); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// cfg.SetUser("turker")
+
+	// cfg, err = config.Read()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// fmt.Println(cfg.CurrentUserName, cfg.DbUrl)
+
 }
