f77069e5a7115cc1a2a12f7eebb0a0f40619dd6e
Türker Kıvılcım <kivturker@gmail.com>
2026-09-07 15:39:56 +0300

    add: implement fetching rss

---
+++ fetchers.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"html"
+	"io"
+	"net/http"
+	"time"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	req.Header.Set("User-Agent", "gator")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var feed RSSFeed
+	if err := xml.Unmarshal(data, &feed); err != nil {
+		return nil, err
+	}
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+	for i, val := range feed.Channel.Item {
+		feed.Channel.Item[i].Title = html.UnescapeString(val.Title)
+		feed.Channel.Item[i].Description = html.UnescapeString(val.Description)
+	}
+
+	return &feed, nil
+}
