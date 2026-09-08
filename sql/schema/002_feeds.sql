c34e89983032770901861d294a27016fdcfeb411
Türker Kıvılcım <kivturker@gmail.com>
2026-09-08 17:40:27 +0300

    fix: change user_id column to allow multiple entries in feeds table

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -5,7 +5,7 @@ CREATE TABLE feeds (
     updated_at TIMESTAMP NOT NULL,
     name VARCHAR(50) NOT NULL,
     url VARCHAR(100) UNIQUE NOT NULL,
-    user_id UUID UNIQUE NOT NULL,
+    user_id UUID NOT NULL,
 
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
